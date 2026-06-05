96b185552273c82449de7c6acdbde8e69733187c
retr0lbb <retr0lbb@gmail.com>
2026-06-05 20:06:51 -0300

    feat: add tray icon

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -34,6 +34,7 @@ pub fn run() {
             let menu = Menu::with_items(app, &[&show, &quit])?;
 
             TrayIconBuilder::new()
+                .icon(app.default_window_icon().unwrap().clone())
                 .menu(&menu)
                 .on_menu_event(|app, event| match event.id.as_ref() {
                     "show" => {
